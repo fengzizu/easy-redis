28489dd6f8bd64fe794c75ca2c8be54d951b8d1e
fengzizu <2225942674@qq.com>
2025-11-14 17:43:29 +0800

    add a test command: PING

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	_ "fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -34,10 +35,33 @@ func main() {
 			return
 		}
 
-		fmt.Println(value)
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
 
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		if !ok {
+			fmt.Println("Invalid command: " + command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+
+		writer.Write(result)
 	}
 
 }
