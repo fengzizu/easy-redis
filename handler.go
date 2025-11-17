b250869fa416f775368a5c67bdf71c2f02bb2e18
fengzizu <2225942674@qq.com>
2025-11-18 00:22:28 +0800

    add more 3 commands: HSET & HGET & HGETALL

--- handler.go
+++ handler.go
@@ -3,9 +3,12 @@ package main
 import "sync"
 
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetall,
 }
 
 var SETMap = map[string]string{}
@@ -50,4 +53,68 @@ func get(args []Value) Value {
 	}
 
 	return Value{typ: "bulk", bulk: value}
+}
+
+var HSETMap = map[string]map[string]string{}
+var HSETLock = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERROR: wrong number of arguments for 'hset' command"}
+	}
+
+	mapKey := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETLock.Lock()
+	if _, ok := HSETMap[mapKey]; !ok {
+		HSETMap[mapKey] = map[string]string{}
+	}
+	HSETMap[mapKey][key] = value
+	HSETLock.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERROR: wrong number of arguments for 'hget' command"}
+	}
+
+	mapKey := args[0].bulk
+	key := args[1].bulk
+	HSETLock.RLock()
+	value, ok := HSETMap[mapKey][key]
+	HSETLock.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERROR: wrong number of arguments for 'hgetall' command"}
+	}
+
+	mapKey := args[0].bulk
+	var result []Value
+
+	HSETLock.RLock()
+	value, ok := HSETMap[mapKey]
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	for key, item := range value {
+		result = append(result, Value{typ: "bulk", bulk: key})
+		result = append(result, Value{typ: "bulk", bulk: item})
+	}
+
+	HSETLock.RUnlock()
+
+	return Value{typ: "array", array: result}
 }
